eb284851ee3fb3c1ba9f160f93f97eb362c0d932
rogalon2000 <128326734+rogalon2000@users.noreply.github.com>
2023-03-24 21:43:26 +0100

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,9 @@ function funkcja2(argument1, argument2)
     }
 
 let poradnia = document.querySelector("#poradnia");
-poradnia.addEventListener("click", funkcja)
+poradnia.addEventListener("click", funkcja)                 /* to się uruchamia grzecznie po klinięciu przycisku */
 
 let przyjecie = document.querySelector("#przyjecie");
-przyjecie.addEventListener("click", funkcja2(c, d))
+przyjecie.addEventListener("click", funkcja2(c, d))         /* to mi się uruchamia przy ładowaniu strony, zamiast po klinięciu przycisku */
+
+/* jak mam zrobić, żeby kliknięcie przycisku uruchamiało funkcję, która przyjmuje jakieś argumenty? */
