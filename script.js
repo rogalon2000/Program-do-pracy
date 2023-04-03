19f2a47259dee5d7aeecedfbf341604a696bd444
rogalon2000 <128326734+rogalon2000@users.noreply.github.com>
2023-04-03 21:47:40 +0200

    całkiem spoko

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+let imieNazwiskoPacjenta = ''
+let peselPacjenta =''
+let daneLekarza =''
+let data=''
 
+/*
+let showHide = function(){
+    let a = document.querySelector('#dane');
+    if (imieNazwiskoPacjenta ==='' && peselPacjenta ==='' && daneLekarza ==='' && data ==='')
+        {
+        a.style.visibility = 'hidden'
+        console.log('dupa')
+        }
+    else
+        {
+        a.style.visibility = 'visible'
+        }
+    }
 
+*/
 przyciskPoradnia = function()
     {
         console.log("uruchomiłeś przycisk od poradni");
@@ -8,6 +26,10 @@ przyciskPoradnia = function()
         a.style.visibility = 'visible'
         let b = document.querySelector('#main2')
         b.style.visibility = 'hidden'
+        let c = document.querySelector('#danePacjent')
+        let d = document.querySelector('#danePESEL')
+        c.style.visibility = 'hidden'
+        d.style.visibility = 'hidden'
     }
 
 przyciskPrzyjecie = function()
@@ -18,6 +40,10 @@ przyciskPrzyjecie = function()
         let b = document.querySelector('#main2')
         b.style.gridColumn = '1/span 4'
         b.style.visibility = 'visible'
+        let c = document.querySelector('#danePacjent')
+        let d = document.querySelector('#danePESEL')
+        c.style.visibility = 'visible'
+        d.style.visibility = 'visible'
     }
 
 let poradnia = document.querySelector("#poradnia");
@@ -26,18 +52,86 @@ poradnia.addEventListener("click", przyciskPoradnia)
 let przyjecie = document.querySelector("#przyjecie");
 przyjecie.addEventListener("click", przyciskPrzyjecie)
 
+let getValue = function(field, mod)
+    {
+    if(mod === 1)
+        {
+        let a = document.querySelector('#danePacjent');
+        imieNazwiskoPacjenta = field.value
+        if (field.value === ''){a.innerHTML=''}
+        else {a.innerHTML='Imię i nazwisko pacjenta: ' + field.value}
+        }
+    else if(mod === 2)
+        {
+        let a = document.querySelector('#danePESEL');
+        peselPacjenta = field.value
+        if (field.value === ''){a.innerHTML=''}
+        else {a.innerHTML='PESEL pacjenta: ' + field.value}
+        }
+    else if(mod === 3)
+        {
+        let a = document.querySelector('#daneLekarz');
+        daneLekarza = field.value
+        if (field.value === ''){a.innerHTML=''}
+        else {a.innerHTML='Dane lekarza: ' + field.value}
+        }
+    else if(mod === 4)
+        {
+        let a = document.querySelector('#daneData');
+        data = field.value
+        if (field.value === ''){a.innerHTML=''}
+        else {a.innerHTML='Data: ' + field.value}
+        }
+    let wartosc = field.value;
+    console.log(wartosc);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 generujPlikiPoradnia = function()
     {
-        console.log('rozpoczynam generowanie plikow')
+        console.log('rozpoczynam generowanie plikow do poradni')
 
 /* tu będzie funkcja służąca do wygenerowania zgód pacjentów na bazie template'a
 po pobraniu danych pacjenta z pliku excel? albo prostszego - wtedy będzie jeszcze funkcja
 do konwersji z excela do prostszych
 */
 
-
-
     }
 
 let generuj = document.querySelector('#generujPlikiPoradnia');
 generuj.addEventListener('click', generujPlikiPoradnia)
+
+
+
+generujPlikiPrzyjecie = function()
+    {
+        console.log('rozpoczynam generowanie plikow do przyjęcia')
+
+/* tu będzie funkcja służąca do wygenerowania dokumentów do przyjęcia do OIT na bazie template'ów
+po pobraniu danych pacjenta z pliku excel? albo prostszego - wtedy będzie jeszcze funkcja
+do konwersji z excela do prostszych
+*/
+
+    }
+
+let generuj2 = document.querySelector('#generujPlikiPrzyjecie');
+generuj2.addEventListener('click', generujPlikiPrzyjecie)
